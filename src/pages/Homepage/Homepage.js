2fa3c9b886f71b525d318357ed7d868274bddbde
mefa <mfbyrktr@gmail.com>
2020-03-29 20:59:05 +0300

    github repo initialized

--- src/pages/Homepage/Homepage.js
+++ src/pages/Homepage/Homepage.js
@@ -2,12 +2,15 @@ import React from 'react';
 
 import MenuItemList from '../../components/Menu-item-list/Menu-item-list.js';
 
-import './Homepage.scss';
+/* import './Homepage.scss'; */
+import { HomePageContainer } from './Homepage.styles.js';
+
+
 
 const HomePage = () => (
-  <div className='homepage'>
+  <HomePageContainer>
     <MenuItemList />
-  </div>
+  </HomePageContainer>
 );
 
 export default HomePage;
